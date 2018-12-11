59ea133f70d15191629aa9d47b923728a5be7d4a
ismatch <ismatch@qq.com>
2018-12-11 23:45:53 +0800

    增加表单上传，测试通过.

---
+++ IsMatch.Test/Test1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IsMatch.Upload;
+using Qiniu.Storage;
+using Xunit;
+
+namespace IsMatch.Test
+{
+    public class Test1
+    {
+        [Fact]
+        public void FormUplaod()
+        {
+            var config = new Config
+            {
+                // 设置上传区域
+                Zone = Zone.ZoneCnEast,
+
+                // 设置 http 或者 https 上传
+                UseHttps = true,
+                UseCdnDomains = true,
+                ChunkSize = ChunkUnit.U512K
+            };
+            string ak = "", sk = "";
+            var upload = new QiNiuUpload(config, ak, sk);
+            var uploadResult = upload.FormUpload("0060lm7Tgy1fistv13d8zj30k00zkgne.jpg");
+            Assert.Contains("hash", uploadResult);
+        }
+    }
+}
---
+++ IsMatch.Upload/QiNiuUpload.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Qiniu.Http;
+using Qiniu.Storage;
+using Qiniu.Util;
+
+namespace IsMatch.Upload
+{
+    public class QiNiuUpload
+    {
+        private Config _Config;
+        private string _AccessKey;
+        private string _SecretKey;
+
+        public QiNiuUpload(Config config, string ak, string sk)
+        {
+            _Config = config;
+            _AccessKey = ak;
+            _SecretKey = sk;
+        }
+
+        public string FormUpload(string filePath)
+        {
+            Mac mac = new Mac(_AccessKey, _SecretKey);
+
+            // 上传文件名
+            string key = "key";
+
+            // 存储空间名
+            string Bucket = "file";
+
+            // 设置上传策略，详见：https://developer.qiniu.com/kodo/manual/1206/put-policy
+            PutPolicy putPolicy = new PutPolicy();
+
+            // 设置要上传的目标空间
+            putPolicy.Scope = Bucket;
+
+            // 上传策略的过期时间(单位:秒)
+            putPolicy.SetExpires(3600);
+
+            // 文件上传完毕后，在多少天后自动被删除
+            putPolicy.DeleteAfterDays = 1;
+
+            // 生成上传token
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+
+            // 表单上传
+            FormUploader target = new FormUploader(_Config);
+            var result = target.UploadFile(filePath, key, token, null).Result;
+            return result.ToString();
+        }
+    }
+}
