87ded22a193082ac4bdf1d853bf6283de1f2c248
ismatch <ismatch@qq.com>
2019-01-04 00:52:57 +0800

    通过调试完成按文件夹归类文件

--- IsMatch.Spider/CourseSource.cs
+++ IsMatch.Spider/CourseSource.cs
@@ -2,7 +2,9 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace IsMatch.Spider
@@ -10,7 +12,10 @@ namespace IsMatch.Spider
     public class CourseSource
     {
         private string ListUrl = "http://www.jinxuliang.com/course2/api/courseService";
-        private string DetailUrl = "http://www.jinxuliang.com/course2/api/courseService/tree/{0}";
+        private string DetailInfoUrl = "http://www.jinxuliang.com/course2/api/courseService/tree/{0}";
+        private string DetailUrl = "http://www.jinxuliang.com/course2/api/pptService/{0}";
+        private string BaseDir = @"E:\Study\CrawlerResult\JinXuLiang";        
+
         public void Work()
         {
             List<Course> courseList = GetList();
@@ -19,6 +24,11 @@ namespace IsMatch.Spider
             GetDetail(courseList);
         }
 
+
+        /// <summary>
+        /// 获取课程列表
+        /// </summary>
+        /// <returns></returns>
         private List<Course> GetList()
         {
             List<Course> listCourse = new List<Course>();
@@ -33,18 +43,137 @@ namespace IsMatch.Spider
             return listCourse;
         }
 
+        /// <summary>
+        /// 获取课程详情
+        /// </summary>
+        /// <param name="courseList"></param>
         private void GetDetail(List<Course> courseList)
         {
             int count = courseList.Count;
-            System.Threading.Tasks.Parallel.For(0, count, (i, state) =>
+            //System.Threading.Tasks.Parallel.For(0, count, (i, state) =>
+            //{
+            //    using (var wc = new WebClient())
+            //    {
+            //        Console.WriteLine("\n" + courseList[i].name + "开始分析...");
+            //        string jsonString = wc.DownloadString(string.Format(DetailInfoUrl, courseList[i]._id));
+            //        CourseDetailInfo detail = JsonConvert.DeserializeObject<CourseDetailInfo>(jsonString);
+            //        if (detail != null && detail.treeData.Length > 0)
+            //        {
+            //            CategoryNameTarget = "";
+            //            ScanChildren(detail.treeData[0]);
+            //        }
+            //    }
+
+            //    Console.WriteLine(courseList[i].name + "download complete...");
+            //});
+
+            foreach (var item in courseList)
             {
-                using(var wc = new WebClient())
+                using (var wc = new WebClient())
                 {
-                    Console.WriteLine("\n" + courseList[i].name + "开始下载...");
-                    var html = wc.DownloadString(string.Format(DetailUrl, courseList[i]._id));
-                    ; Console.WriteLine("\n" + courseList[i].name + "\n返回JSON" + html);
+                    Console.WriteLine("\n" + item.name + "开始分析...");
+                    string jsonString = wc.DownloadString(string.Format(DetailInfoUrl, item._id));
+                    CourseDetailInfo detail = JsonConvert.DeserializeObject<CourseDetailInfo>(jsonString);
+                    if (detail != null && detail.treeData.Length > 0)
+                    {
+                        ScanChildren(detail.treeData[0]);
+                    }
                 }
-            });
+
+                Console.WriteLine(item.name + "download complete...");
+            }
+        }
+
+        // 记录当前路径
+        private string CategoryNameTarget = "";
+
+        /// <summary>
+        /// 提取课程id
+        /// </summary>
+        private void ScanChildren(Treedata children)
+        {
+            if (children.children.Length > 0)
+            {
+                // 记录当前路径
+                if(!CategoryNameTarget.Contains(children.name)) CategoryNameTarget += "\\" + children.name;
+
+                // 遍历每一次子目录
+                foreach (var childrenChild in children.children)
+                {
+                    // 记录当前路径
+                    if (!CategoryNameTarget.Contains(childrenChild.name)) CategoryNameTarget += "\\" + childrenChild.name;
+                    ScanChildren(childrenChild);
+                }
+
+                // 回退到上一路径
+                CategoryNameTarget = CategoryNameTarget.Replace("\\" + children.name, "");
+            }
+            else
+            {
+                DownloadPowerPointAndCode(children.nodeId, CategoryNameTarget);
+
+                // 回退到上一路径
+                CategoryNameTarget = CategoryNameTarget.Replace("\\" + children.name, "");
+            }
+        }
+
+        /// <summary>
+        /// 下载PPT和源码
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="categoryName"></param>
+        private void DownloadPowerPointAndCode(string id, string categoryName)
+        {
+            try
+            {
+                string directoryName = BaseDir + categoryName;
+                if (!Directory.Exists(directoryName))
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+                using (var wc = new WebClient())
+                {
+                    Console.WriteLine("\n" + id + "开始下载...");
+                    string jsonString = wc.DownloadString(string.Format(DetailUrl, id));
+                    CourseDetail detail = JsonConvert.DeserializeObject<CourseDetail>(jsonString);
+
+                    if (!string.IsNullOrWhiteSpace(detail.sourceCodeDownloadURL))
+                    {
+                        var name = "\\" + Path.GetFileName("http://www.jinxuliang.com" + detail.sourceCodeDownloadURL);
+                        wc.DownloadFile("http://www.jinxuliang.com" + detail.sourceCodeDownloadURL, directoryName + name);
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(detail.originalFileDownloadURL))
+                    {
+                        var name = "\\" + Path.GetFileName("http://www.jinxuliang.com" + detail.originalFileDownloadURL);
+                        wc.DownloadFile("http://www.jinxuliang.com" + detail.originalFileDownloadURL, directoryName + name);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("下载PPT和源码发生错误，原因：" + ex.Message);
+            }
+        }
+
+        private string ModFilePath(string filePath)
+        {
+         
+            StringBuilder rBuilder = new StringBuilder(filePath);
+            foreach (char rInvalidChar in Path.GetInvalidFileNameChars())
+            {
+                rBuilder.Replace(rInvalidChar.ToString(), string.Empty);
+            }
+
+            rBuilder = new StringBuilder(rBuilder.ToString());
+            foreach (char rInvalidChar in Path.GetInvalidPathChars())
+            {
+                rBuilder.Replace(rInvalidChar.ToString(), string.Empty);
+
+            }
+
+            return rBuilder.ToString();
         }
     }
 }
---
+++ IsMatch.Spider/Models/CourseDetailInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsMatch.Spider.Models
+{
+    public class CourseDetailInfo
+    {
+        public string courseId { get; set; }
+        public string name { get; set; }
+        public Treedata[] treeData { get; set; }
+    }
+
+    public class Treedata
+    {
+        public string nodeId { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+        public Treedata[] children { get; set; }
+        public bool selected { get; set; }
+        public bool disable { get; set; }
+        public bool expanded { get; set; }
+    }
+
+
+    public class CourseDetail
+    {
+        public string _id { get; set; }
+        public string rootVirtualPath { get; set; }
+        public string originalFileDownloadURL { get; set; }
+        public string sourceCodeDownloadURL { get; set; }
+        public string resourceGuid { get; set; }
+        public bool isWideScreen { get; set; }
+        public string description { get; set; }
+        public string name { get; set; }
+        public int difficultyLevel { get; set; }
+        public int viewCount { get; set; }
+        public int voteCount { get; set; }
+        public object[] comments { get; set; }
+    }
+
+}
