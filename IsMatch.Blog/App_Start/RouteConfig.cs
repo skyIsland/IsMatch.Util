89087e05118de7cbe859611f2c3faea08e663c9b
ismatch <ismatch@qq.com>
2019-01-06 00:05:55 +0800

    完成基本功能，了解一下dbutilliy的使用。

--- IsMatch.Blog/App_Start/RouteConfig.cs
+++ IsMatch.Blog/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace IsMatch.Blog
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{action}/{id}/{p}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, p = 1 }
             );
         }
     }
--- IsMatch.Blog/Controllers/HomeController.cs
+++ IsMatch.Blog/Controllers/HomeController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ivony.Data;
 
 namespace IsMatch.Blog.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        // GET: Home
-        public ActionResult Index()
+        [Route("{p:int?}")]
+        public ActionResult Index(int p = 1)
         {
-            return View();
+            var posts = _db.T($"SELECT * FROM (SELECT *,ROW_NUMBER() OVER (ORDER BY ID DESC) rowId FROM dbo.Posts) AS b WHERE b.rowId BETWEEN -1 * {p} AND {p * 5}").ExecuteDataTable();
+            return View(posts);
+        }
+
+        [Route("catalog/{id}/{p:int?}")]
+        public ActionResult Catalog(string id, int p = 1)
+        {
+            // 查找分类
+            var catalog = _db.T("SELECT TOP 1 * FROM dbo.Catalogs WHERE Url = '" + id + "'").ExecuteFirstRow();
+            if (catalog == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Title = catalog["Title"];
+
+            // 页面直接转跳
+            if (catalog["IsPage"].ToString() == "1")
+            {
+                return Redirect(catalog["Url"].ToString());
+            }        
+            
+            // 下一页地址
+            ViewBag.PageUrl = "" + id + "/" + (p + 1);
+
+            // rows number分页 BETWEEN 当前页数 -1 * 条数 and 页数 * 条数     
+            var posts = _db.T($"SELECT * FROM (SELECT *,ROW_NUMBER() OVER (ORDER BY ID DESC) rowId FROM dbo.Posts WHERE CatalogId = {catalog["ID"]}) AS b WHERE b.rowId BETWEEN -1 * {p} AND {p * 5}").ExecuteDataTable();
+            return View("Index", posts);
         }
     }
 }
