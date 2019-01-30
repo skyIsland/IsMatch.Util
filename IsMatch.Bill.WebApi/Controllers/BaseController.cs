42ca09b4795d4b2494c6d66e6008b2c96cc987cd
ismatch <ismatch@qq.com>
2019-01-30 22:08:40 +0800

    添加项目。

---
+++ IsMatch.Bill.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsMatch.Bill.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
