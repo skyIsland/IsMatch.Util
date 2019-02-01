c2cbedbac8de7f4b83e56dc5d95dda86c839a42e
ismatch <ismatch@qq.com>
2019-02-01 23:26:50 +0800

    code first

--- IsMatch.Bill.WebApi/Controllers/BaseController.cs
+++ IsMatch.Bill.WebApi/Controllers/BaseController.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IsMatch.Bill.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IsMatch.Bill.WebApi.Controllers
 {
     [Route("api/[controller]")]
-    public class BaseController : Controller
+    public class BaseController : ControllerBase
     {
-        public IActionResult Index()
+        protected EfDbContext _context;
+        public BaseController(EfDbContext context)
         {
-            return View();
-        }
+            this._context = context;
+        }       
     }
 }
---
+++ IsMatch.Bill.WebApi/Models/EFDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace IsMatch.Bill.WebApi.Models
+{
+    public class EfDbContext : DbContext
+    {
+        public EfDbContext(DbContextOptions<EfDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BillInfo> BillInfo { get; set; }
+    }
+}
