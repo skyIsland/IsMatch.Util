d8d35e27088da0c7992a6f319dab38a3430fcff1
ismatch <ismatch@qq.com>
2019-03-07 19:04:50 +0800

    重构Blog

--- IsMatch.Blog/Controllers/BaseController.cs
+++ IsMatch.Blog/Controllers/BaseController.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Ivony.Data;
-using Ivony.Data.SqlClient;
+
 
 namespace IsMatch.Blog.Controllers
 {
     public class BaseController : Controller
     {
-        protected SqlDbExecutor _db;
-
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            _db = SqlServer.FromConfiguration("Conn");
-            Prepare();            
             base.OnActionExecuting(filterContext);
         }
 
         public virtual void Prepare()
         {
-            ViewBag.Catalogs = _db.T("SELECT * FROM Catalogs").ExecuteDataTable();
+           
         }
 
         public ActionResult NotFound()
