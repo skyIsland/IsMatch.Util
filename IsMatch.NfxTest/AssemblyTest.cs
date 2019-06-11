4bc5e0c91808b442fefbcdb09cd93d3446f8b600
IsMatch <ismatch@qq.com>
2019-06-11 10:33:04 +0800

    测试。

---
+++ IsMatch.NfxTest/AssemblyTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsMatch.NfxTest
+{
+    public static class AssemblyTest
+    {
+        public static string DefaultEntityName { get; private set; }
+
+        static AssemblyTest()
+        {
+            DefaultEntityName = "IsMatch.CnArticleSubscribe";
+        }
+
+        public static string GetEntityType(string entityName)
+        {
+            var type = Assembly.Load(DefaultEntityName).GetType(entityName);
+
+            if(type == null)
+            {
+                throw new ArgumentException(nameof(entityName));
+            }
+
+            return type.ToString();
+        }
+    }
+}
---
+++ IsMatch.NfxTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsMatch.NfxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+
+            Console.ReadKey();
+        }
+
+        private static void Test1()
+        {
+            AssemblyTest.GetEntityType("MailHelper");
+        }
+    }
+}
