5326654f3aa8ab0b8c7d7b80c591b8e053b94f0d
ismatch <ismatch@qq.com>
2019-07-22 00:50:17 +0800

    FriendlyDetail

--- IsMatch.Spider/Txt/BiQuGe.cs
+++ IsMatch.Spider/Txt/BiQuGe.cs
@@ -22,6 +22,13 @@ namespace IsMatch.Spider.Txt
         //{
 
         //}
+        public static string ReplaceSpecial(string str)
+        {
+            return str.Replace("天才一秒记住本站地址：[笔趣阁]", "")
+                    .Replace("https://www.ibiquge.net/最快更新！无广告！", "")
+                    .Replace("章节错误,点此报送(免注册),", "")
+                    .Replace("报送后维护人员会在两分钟内校正章节内容,请耐心等待。", "");
+        }
 
         /// <summary>
         /// 给详情内容重新排序和去掉某些文字
@@ -34,6 +41,31 @@ namespace IsMatch.Spider.Txt
             //    item.Value = item.Value.Replace(@"                    天才一秒记住本站地址：[笔趣阁]
             //    https://www.ibiquge.net/最快更新！无广告！","");
             //}
+
+            // TODO:两个字典，第二个字典如何按照第二个字典的key进行排序？
+            //_DetailContext = _DetailContext.OrderBy(p =>
+            //{
+            //    var array = System.Text.Encoding.ASCII.GetBytes(p.Key);
+            //    string asciiStr2 = null;
+            //    for (int i = 0; i < array.Length; i++)
+            //    {
+            //        int asciiCode = (int)(array[i]);
+            //        asciiStr2 += Convert.ToString(asciiCode);
+            //    }
+            //    return asciiStr2;
+            //}).ToDictionary(o => o.Key, p => p.Value);
+
+            var newDic = new Dictionary<string, string>();
+            foreach (var detail in _ListUrl)
+            {
+                var detailKey = detail.Key;
+                if (_DetailContext.ContainsKey(detailKey))
+                {
+                    newDic.Add(detailKey, ReplaceSpecial(_DetailContext[detailKey]));
+                }
+            }
+
+            _DetailContext = newDic;
         }
 
         public void Start()
@@ -62,9 +94,9 @@ namespace IsMatch.Spider.Txt
                 {
                     base._DetailContext.Add(title, elemt?.TextContent);
                 }
-            }, 
+            },
              OutPutMsg
-            );           
+            );
 
             // 输出文本文件
             base.OutPutTxt(OutPutMsg);
