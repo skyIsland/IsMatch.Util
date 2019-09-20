a9f61f4c1dbaeef8a01b555e1f16e9d6eb0b392a
IsMatch <ismatch@qq.com>
2019-09-20 12:59:16 +0800

    立新FLag

---
+++ IsMatch.StoriesDownloader/Provider/IStoriesProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsMatch.StoriesDownloader.Provider
+{
+    public interface IStoriesProvider
+    {
+        string ProviderName { get; set; }
+
+        /// <summary>
+        /// 搜索
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <param name="pageNo"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        List<string> SearchStories(string keyword, int pageNo, int pageSize);
+
+
+    }
+}
