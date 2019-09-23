1b61df4f9301b617db58d62f814087037ae57cdf
IsMatch <ismatch@qq.com>
2019-09-23 16:35:26 +0800

    model

---
+++ IsMatch.StoriesDownloader/Models/Story.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsMatch.StoriesDownloader.Models
+{
+    public class Story
+    {
+        /// <summary> 名字 </summary>
+        public string Title { get; set; }
+
+        /// <summary> 地址前缀 </summary>
+        public string UrlStart { get; set; }
+
+        /// <summary> 首页地址 </summary>
+        public string IndexUrl { get; set; }
+
+        /// <summary> 描述 </summary>
+        public string Description { get; set; }        
+
+        /// <summary> 列表规则 </summary>
+        public string ListRule { get; set; }
+
+        /// <summary> 内容规则 </summary>
+        public string DetailRule { get; set; }
+
+    }
+}
