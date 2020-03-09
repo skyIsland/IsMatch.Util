ef9195b12f7266f4ce0d35de731ae6f9cda00bce
IsMatch <ismatch@qq.com>
2020-03-09 14:11:17 +0800

    update

--- IsMatch.Spider/Common/CommonDownload.cs
+++ IsMatch.Spider/Common/CommonDownload.cs
@@ -77,32 +77,32 @@ namespace IsMatch.Spider.Common
 
 
                 #region 开线程下载 10个链接开一个线程
-                if (_Rule.IsMutilThread)
+                //if (_Rule.IsMutilThread)
+                //{
+                int maxCountThread = _Rule.IsMutilThread ? 10 : 1;
+                var threadCount = (int)Math.Ceiling((double)_ListUrl.Count / maxCountThread);
+                Task[] tasks = new Task[threadCount];
+                for (int i = 0; i < threadCount; i++)
                 {
-                    int maxCountThread = 1;
-                    var threadCount = (int)Math.Ceiling((double)_ListUrl.Count / maxCountThread);
-                    Task[] tasks = new Task[threadCount];
-                    for (int i = 0; i < threadCount; i++)
+                    var data = _ListUrl.ToList().Skip(i * maxCountThread).Take(maxCountThread).ToList();
+                    tasks[i] = Task.Factory.StartNew(() =>
                     {
-                        var data = _ListUrl.ToList().Skip(i * maxCountThread).Take(maxCountThread).ToList();
-                        tasks[i] = Task.Factory.StartNew(() =>
+                        foreach (var detailUrl in data)
                         {
-                            foreach (var detailUrl in data)
+                            var document = GetHtmlDocumet(_Rule.UrlStart + detailUrl.Value);
+                            var detailHtml = document.QuerySelector(selectors);
+
+                            if (detailHtml != null)
                             {
-                                var document = GetHtmlDocumet(_Rule.UrlStart + detailUrl.Value);
-                                var detailHtml = document.QuerySelector(selectors);
+                                htmlCollection?.Invoke(detailHtml, detailUrl.Key);
 
-                                if (detailHtml != null)
-                                {
-                                    htmlCollection?.Invoke(detailHtml, detailUrl.Key);
-
-                                    WriteLog?.Invoke($"{detailUrl.Key}获取成功！");
-                                }
-                                else
-                                {
-                                    WriteLog?.Invoke($"{detailUrl.Key}获取失败！");
-                                }
+                                WriteLog?.Invoke($"{detailUrl.Key}获取成功！");
                             }
+                            else
+                            {
+                                WriteLog?.Invoke($"{detailUrl.Key}获取失败！");
+                            }
+                        }
 
                             //foreach (var list in data)
                             //{
@@ -112,33 +112,33 @@ namespace IsMatch.Spider.Common
                             //    var detailStr =
                             //}
                         });
-                    }
-                    Task.WaitAll(tasks/*, TimeSpan.FromSeconds(2000)*/);
                 }
-                else
-                {
-                    foreach (var detailUrl in _ListUrl)
-                    {
-                        var document = GetHtmlDocumet(_Rule.UrlStart + detailUrl.Value);
-                        var detailHtml = document.QuerySelector(selectors);
+                Task.WaitAll(tasks/*, TimeSpan.FromSeconds(2000)*/);
+                //}
+                //else
+                //{
+                //    foreach (var detailUrl in _ListUrl)
+                //    {
+                //        var document = GetHtmlDocumet(_Rule.UrlStart + detailUrl.Value);
+                //        var detailHtml = document.QuerySelector(selectors);
 
-                        htmlCollection?.Invoke(detailHtml, detailUrl.Key);
+                //        htmlCollection?.Invoke(detailHtml, detailUrl.Key);
 
-                        if (detailHtml != null)
-                        {
-                            htmlCollection?.Invoke(detailHtml, detailUrl.Key);
+                //        if (detailHtml != null)
+                //        {
+                //            htmlCollection?.Invoke(detailHtml, detailUrl.Key);
 
-                            WriteLog?.Invoke($"{detailUrl.Key}获取成功！");
-                        }
-                        else
-                        {
-                            WriteLog?.Invoke($"{detailUrl.Key}获取失败！");
-                        }
+                //            WriteLog?.Invoke($"{detailUrl.Key}获取成功！");
+                //        }
+                //        else
+                //        {
+                //            WriteLog?.Invoke($"{detailUrl.Key}获取失败！");
+                //        }
 
-                        Thread.Sleep(1500);
-                    }
+                //        Thread.Sleep(1500);
+                //    }
 
-                }
+                //}
 
                 #endregion
             }
